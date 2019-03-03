0e60f623834966584020e0072244b4e061c7dc30
Arkadiusz Kalka <arkadiusz.kalka@gmail.com>
2019-03-03 19:26:17 +0100

    Add @MethodSource argument provider test for FizzBuzz class

--- src/test/java/com/infoshareacademy/junit/task2/FizzBuzzTest.java
+++ src/test/java/com/infoshareacademy/junit/task2/FizzBuzzTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -64,36 +65,54 @@ public class FizzBuzzTest {
     }
 
     @DisplayName("Should return Fizz if number is divisible by three")
-    @ParameterizedTest(name="{index}  {0} should by divided by 3" )
-    @ValueSource(ints = { 3, 6, 9, 99})
+    @ParameterizedTest(name = "{index}  {0} should by divided by 3")
+    @ValueSource(ints = {3, 6, 9, 99})
     public void verifyIfReturnBuzzWhenDdivisableByThree(int number) {
 
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String expectedResult="Fizz!";
+        String expectedResult = "Fizz!";
         String actualResult = fizzBuzz.play(number);
 
-        assertEquals(expectedResult,actualResult);
+        assertEquals(expectedResult, actualResult);
     }
 
 
     @DisplayName("Should return Buzz if number is divisible by five")
-    @ParameterizedTest(name="{index} - value {0}" )
+    @ParameterizedTest(name = "{index} - value {0}")
     @MethodSource("dataProvider")
     public void shouldReturnBuzzWhenDivisableBy5(int number) {
 
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String expectedResult="Buzz!";
+        String expectedResult = "Buzz!";
         String actualResult = fizzBuzz.play(number);
 
-        assertEquals(expectedResult,actualResult);
+        assertEquals(expectedResult, actualResult);
     }
-    private static Stream<Integer> dataProvider(){
+
+    private static Stream<Integer> dataProvider() {
         return Stream.of(5, 20, 110, 205, 10000);
 
-
     }
 
+    @DisplayName("Should return expected value")
+    @ParameterizedTest(name = "{index} - for {0} should return {1}")
+    @MethodSource("argumentProvider")
+    public void shouldReturnExpectedValue(int number, String expectedResult) {
 
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actualResult = fizzBuzz.play(number);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    private static Stream<Arguments> argumentProvider() {
+        return Stream.of(
+                Arguments.arguments(1, "1"),
+                Arguments.arguments(3, "Fizz!"),
+                Arguments.arguments(5, "Buzz!"),
+                Arguments.arguments(15, "FizzBuzz!")
+
+        );
+    }
 
 }
 
